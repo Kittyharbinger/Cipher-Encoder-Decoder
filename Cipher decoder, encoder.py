dee7a92c87b432a1c6706901978bc7adeb9d6a80
Kittyharbinger <167820815+Kittyharbinger@users.noreply.github.com>
2025-06-29 09:30:50 +0100

    put old lines in separate py, branch learnin next

--- Cipher decoder, encoder.py
+++ Cipher decoder, encoder.py
@@ -2,12 +2,6 @@
 
 # https://cryptii.com/pipes/caesar-cipher
 
-# Lists
-#letters_replacing = []
-#letter_replacements = []
-#replacement_candidates = []
-#replaced_by_candidates = []
-
 # DICTIONARY
 letter_replacements = {} # can update, no dupes, pairs. perfect for this
 
@@ -195,21 +189,6 @@ hswohila, aoha uva h dvyk jvbsk il thkl vba.
 
 
 # DUMPING GROUND #
-
-# 2 lists idea before dictionary #
-#replaced_by_candidates.append(replacing)
-#replacement_candidates.append(replacement)
-#print("success candidate:", replacement_candidates, "letter replaced:",replaced_by_candidates) # list grows??
-#print("choice:",choice)
-
-#letter_replacements["letter plz"] = "replacement letter" # store letter as var
-#print(letter_replacements)
-
-# theorising
-# cycle back through pairings, or list with indexes attached, add to success list
-# or letter menu: mark replacement success, fail, replace a letter
-# get index of "x" in list, if not, say "hasn't been used"
-
 
 # Y/N BUG #
 # Y code doesn't print anything at all, N code uses Y code with choice: n???
---
+++ archived lines cipher.py
@@ -0,0 +1,20 @@
+# Lists
+#letters_replacing = []
+#letter_replacements = []
+#replacement_candidates = []
+#replaced_by_candidates = []
+
+
+# 2 lists idea before dictionary #
+#replaced_by_candidates.append(replacing)
+#replacement_candidates.append(replacement)
+#print("success candidate:", replacement_candidates, "letter replaced:",replaced_by_candidates) # list grows??
+#print("choice:",choice)
+
+#letter_replacements["letter plz"] = "replacement letter" # store letter as var
+#print(letter_replacements)
+
+# theorising
+# cycle back through pairings, or list with indexes attached, add to success list
+# or letter menu: mark replacement success, fail, replace a letter
+# get index of "x" in list, if not, say "hasn't been used"

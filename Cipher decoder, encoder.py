47ef3b53e921e973e033efc12f2db0961c55515f
Kittyharbinger <167820815+Kittyharbinger@users.noreply.github.com>
2025-07-03 21:18:57 +0100

    updating branch, why'd it retain super old stuff?

--- Cipher decoder, encoder.py
+++ Cipher decoder, encoder.py
@@ -2,18 +2,27 @@
 
 # https://cryptii.com/pipes/caesar-cipher
 
-# Dictionary, {replaced:replacement}
-letter_replacements = {} # can update, no dupes, pairs. perfect for this
+# TODO include example ciphers (YT comments, wikipedia)
 
-# Text
+# TODO match candidate_words index with letter_replacements
+# make relationship clear, number them in print()
+# for loop, nested loop inside to print candidate word
+# history of changes? pages? what's the most helpful?
+
+# (realising this ^ is a UI, huh?)
+
+
+# Cipher text
 original_text = ""
 decoding_text = ""
 decoding_text_prev = ""
 #decoded_text = ""
 
 # Letter vars
+# TODO change language, brevity
 replacing = ""
 replacement = ""
+letters_replaced = [] # changed to list, dict keys overwritten. bad idea
 
 # Candidate words
 candidate_words = []
@@ -21,17 +30,17 @@ candidate_words = []
 
 print("Welcome to the Decoder/Encoder")
 
-
 def start():
     print("What would you like to do?")
-    print("1. Decode cipher\n2. Encode cipher")
+    print("1. Decode cipher\n2. Encode cipher (coming soon(TM))")
     choice = input("> ")
 
     if choice == "1":
-        #decoding_cipher_options()
-        cipher_on_hand()
+        decoding_cipher_options()
+
     elif choice == "2":
-        print("encoding not availble atm (enter anything to continue)")
+        # figure out caesar shift, shift substring by x amount of letters 
+        print("Encoding is not available yet (enter anything to continue)")
         input("> ")
         cipher_on_hand()
 
@@ -42,7 +51,6 @@ def decoding_cipher_options():
     print("Decode example cipher? y/n (press Enter to skip)")
     choice = input("> ")
     if choice == "y":
-        # grab YT comments, wikipedia excerpts
         print("no examples yet (enter anything to continue)")
         input("> ")
         
@@ -58,12 +66,11 @@ def decoding_cipher_options():
 
 
 def cipher_on_hand():
-    # give example ciphers option, Enter to skip
-    print("Please provide text to decode (please use lower case for now)") # code breaks here
+    print("Please provide text to decode (please use lower case for now)") # if replacing.upper() in text, replacement.upper()
     global original_text
     original_text = input("> ")
 
-    global encoded_text
+    global encoded_text # same as original text?
     encoded_text = original_text
 
     global decoding_text
@@ -80,28 +87,37 @@ def substitution_time():
     print("---")
     print(f"Cipher text: {decoding_text}")
     print("Type letter to be replaced (please use lower case for now)")
+    print("If you'd like to restart, type \"res\" or \"restart\"")
 
     global replacing
     replacing = input("> ")
 
-    if replacing == " ": # just "" is interpreted as str()
+    if len(replacing) < 1:
         substitution_time()
     elif len(replacing) > 1:
-        substitution_time()
-    elif replacing not in decoding_text: # unlikely, failsafe anyway
+        if replacing == "res" or "restart": # BUG anything between "res" and "restart" triggers func
+            restart_decoding()
+        else:
+            substitution_time()
+
+    elif replacing not in decoding_text: # failsafe
         print("letter not found in text (enter anything to continue)")
         input("> ")
         substitution_time()
 
 
-    print("Type a replacement letter (please use lower case for now)")
+    print("Type a replacement letter (please use lower case for now) (If you'd like to restart, type \"res\" or \"restart\")")
     global replacement
     replacement = input("> ")
 
-    if replacement == " ":
+    if len(replacement) < 1: 
         substitution_time()
     elif len(replacement) > 1:
-        substitution_time()
+        if replacement == "res" or "restart":
+            restart_decoding()
+        else:
+            substitution_time()
+        
 
     global decoding_text_prev
     decoding_text = decoding_text.replace(replacing, replacement)
@@ -115,32 +131,37 @@ def proceed_or_undo():
     print("Proceed with change or undo? \n1. Proceed \n2. Undo")
     choice = input("> ")
     if choice == "1":
-        letter_replacements["replaced: " + replacing] = ("replacement: " + replacement) # success :D
-        print(letter_replacements)
+        letters_replaced.append(f"replaced: {replacing}, replacement: {replacement}")
+        print(letters_replaced)
         overwrite_prev()
 
     elif choice == "2":
         undo_replacement()
-        #print("choice:",choice)
         
     else:
         proceed_or_undo()
     
-
+    
 def overwrite_prev():
     global decoding_text
     global decoding_text_prev
     decoding_text_prev = decoding_text
     ask_add_candidate()
 
+def undo_replacement():
+    global decoding_text
+    global decoding_text_prev
+    decoding_text = decoding_text_prev
+
+    substitution_time()
+
 
 def ask_add_candidate():
-    # testing, how branches work, merge back etc.
     print("Store candidate words containing the replacement letter? y/n")
     choice2 = input("> ")
 
     if choice2 == "y":
-        add_candidate_words()      
+        add_candidate_words()
 
     elif choice2 == "n":
         print("no candidate found")
@@ -150,7 +171,6 @@ def ask_add_candidate():
         ask_add_candidate()
 
 
-
 def add_candidate_words():
     text_copy = ""
     global decoding_text
@@ -158,60 +178,55 @@ def add_candidate_words():
 
     global replacement
 
-    # ask user for each word? select which numbers they want to keep? # feature creep?
+    # ask user for each word? select which indices they want to keep? # feature creep?
     for x in text_copy.split(" "):
         if replacement in x:
-            # append as a list, have multiple in 1 index 
-            candidate_words.append(x) # hell yeah first try
+            
+            candidate_words.append(x) # works now, missed split(" ") before
             
 
-    #print(candidate_words)
-    print(f"candidate words: {candidate_words}") # pages?
+    print(f"candidate words: {candidate_words}") # TODO match index with letter_replacements
     input("> ")
-
-    # candidate list, var name to keep parity with replacement dict?
     
     substitution_time()
 
 
-def undo_replacement():
-    #print("UNDO coming soon")
+
+
+
+def restart_decoding():
     global decoding_text
-    global decoding_text_prev
-    decoding_text = decoding_text_prev
-
-    # leapfrog vars, revert text to prev version
-
+    global original_text
+    decoding_text = original_text
+    print("decoding restarted")
     substitution_time()
-
-
 
 start()
 
-# TODO include example ciphers
+
+# Spitballing
+# notify user of letter with highest count? like frequency analysis
+
+
+# Example ciphers #
 """
 Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol 
 hswohila, aoha uva h dvyk jvbsk il thkl vba.
-
 """
-
-# notify user of letter with highest count? like frequency analysis
-
-# BUG can change letters into one that's still encoded (e.g a->h) and uhhh shit
-# TODO undo change, store candidate pairs, restart decoding
-# dictionary and a candidate word
-
-# realising this ^ is a UI huh
+# grab YT comments, wikipedia excerpts
 
 
 
 
-# dict list? #
-#decoded_text_step = ""
-#global decoded_text = decoded_text_step
+# EXPLANATION GROUND (/bugs fixed (isnt that what commit history is for?)) #
+# blank input bug
+# if replacement == " ": # BUG deletes character, dang it
+# replacing " " was accepted too
+# fixed with len() > or < 1
 
+# Undo feature #
+# leapfrog vars, revert text to prev version
 
-# DUMPING GROUND #
 
 # Y/N BUG #
 # Y code doesn't print anything at all, N code uses Y code with choice: n???
